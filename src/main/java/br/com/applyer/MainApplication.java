77b0c123b1e80b675e2843725ef96115eb9176d3
unknown <macedo.rafanazareth@gmail.com>
2019-09-28 15:54:21 -0300

    change to create-drop

--- src/main/java/br/com/applyer/MainApplication.java
+++ src/main/java/br/com/applyer/MainApplication.java
@@ -2,10 +2,13 @@ package br.com.applyer;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class MainApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/applyer/v2/vaga/controller/VagaController.java
+++ src/main/java/br/com/applyer/v2/vaga/controller/VagaController.java
@@ -6,7 +6,6 @@ import br.com.applyer.v2.vaga.entity.Vaga;
 import br.com.applyer.v2.vaga.service.VagaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
