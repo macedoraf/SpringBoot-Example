8f8d54933db0243064eacaded5bca265fdbbd1a0
unknown <macedo.rafanazareth@gmail.com>
2019-10-01 20:29:41 -0300

    Added some unitTests

--- src/test/java/br/com/applyer/demo/service/RequisitoServiceTest.java
+++ src/test/java/br/com/applyer/demo/service/RequisitoServiceTest.java
@@ -1,16 +1,51 @@
 package br.com.applyer.demo.service;
 
+import br.com.applyer.v2.service.RequisitorService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static br.com.applyer.demo.Mocks.getMockedReq;
+import static br.com.applyer.demo.Mocks.getMockedVaga;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RequisitoServiceTest {
 
-    @Test
-    public void test(){
+    @Autowired
+    private RequisitorService service;
 
+    @Test
+    public void salvar(){
+        service.salvar(getMockedReq());
+    }
+
+    @Test
+    public void update(){
+        Long id = service.salvar(getMockedReq()).getId();
+        service.atualizar(id,getMockedReq());
+    }
+
+    @Test
+    public void deletar(){
+        Long id = service.salvar(getMockedReq()).getId();
+        service.deletar(id);
+    }
+
+    @Test
+    public void listar(){
+        service.salvar(getMockedReq());
+        service.salvar(getMockedReq());
+        service.salvar(getMockedReq());
+
+        service.listarTodos();
+    }
+
+    @Test
+    public void buscarPorId(){
+        Long id = service.salvar(getMockedReq()).getId();
+        service.buscaPeloId(id);
     }
 }
--- src/test/java/br/com/applyer/demo/service/VagaServiceTest.java
+++ src/test/java/br/com/applyer/demo/service/VagaServiceTest.java
@@ -1,17 +1,51 @@
 package br.com.applyer.demo.service;
 
+import br.com.applyer.v2.service.VagaService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static br.com.applyer.demo.Mocks.getMockedVaga;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class VagaServiceTest {
 
-    @Test
-    public void test(){
+    @Autowired
+    private VagaService vagaService;
 
+    @Test
+    public void salvar(){
+        vagaService.salvar(getMockedVaga());
+    }
+
+    @Test
+    public void atualizar(){
+        Long id = vagaService.salvar(getMockedVaga()).getId();
+        vagaService.atualizar(id,getMockedVaga());
+    }
+
+    @Test
+    public void deletar(){
+        Long id = vagaService.salvar(getMockedVaga()).getId();
+        vagaService.deletar(id);
+    }
+
+    @Test
+    public void buscarId(){
+        Long id = vagaService.salvar(getMockedVaga()).getId();
+        vagaService.buscaPeloId(id);
+    }
+
+    @Test
+    public void listAll(){
+        vagaService.salvar(getMockedVaga());
+        vagaService.salvar(getMockedVaga());
+        vagaService.salvar(getMockedVaga());
+        vagaService.salvar(getMockedVaga());
+        vagaService.listarTodos();
     }
 
 }
